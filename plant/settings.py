6e860607cb6ef476e82c3132323aaa8cf4602fbd
user-fail <huahualihu@gmail.com>
2024-05-26 15:16:58 +0800

    django

--- plant/settings.py
+++ plant/settings.py
@@ -108,7 +108,7 @@ import os
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'plant_query/static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
